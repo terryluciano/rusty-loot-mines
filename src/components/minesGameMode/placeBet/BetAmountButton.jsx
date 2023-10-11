edc19c9ab5dd724d72d5a2ac68beddba8aceb738
Terry Luciano <tergeek@gmail.com>
2023-10-10 23:43:41 -0400

    GitHub Pages Branch

--- src/components/minesGameMode/placeBet/BetAmountButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function BetAmountButton(props) {
-	return (
-		<button class='bet-amount-button h-6 rounded border-white/[.08] border flex-shrink-0'>
-			{props.value}
-		</button>
-	);
-}
